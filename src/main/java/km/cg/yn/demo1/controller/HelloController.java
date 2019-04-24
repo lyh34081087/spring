2b76e665dad8c0a00f572c3a4a8ad79aeabdc5ea
lyh <34081087@qq.com>
2019-04-24 22:48:24 +0800

    first commit

---
+++ src/main/java/km/cg/yn/demo1/controller/HelloController.java
@@ -0,0 +1,24 @@
+package km.cg.yn.demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈hello〉
+ *
+ * @author Administrator
+ * @create 2019-4-24
+ * @since 1.0.0
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name , Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
